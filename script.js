f3fe03f09c95a2e1aaa31c2b60041102409289cc
Chris Rogers <47615352+chrislrogers@users.noreply.github.com>
2023-04-10 21:06:21 +0100

    inital commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const menuButton = document.getElementsByClassName('menu-button')[0]
+const navLinks = document.getElementsByClassName('nav-links')[0]
+
+menuButton.addEventListener('click', () => {
+  navLinks.classList.toggle('active')
+})
